110c45ab8bd728ed37bd0e1b445fe19ae86bcfea
abdulmughnialfikri <dev.abdulmughni@gmail.com>
2020-07-15 04:50:15 +0700

    layout login

--- app/src/main/java/id/co/personal/pasarikan/MyFunction.kt
+++ app/src/main/java/id/co/personal/pasarikan/MyFunction.kt
@@ -4,8 +4,28 @@ import android.content.Context
 import android.graphics.Color
 import android.widget.Toast
 import cn.pedant.SweetAlert.SweetAlertDialog
+import com.google.firebase.auth.FirebaseAuth
+
 
 object MyFunction {
+    fun getCurrentUser(){
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = user.displayName
+            val email = user.email
+            val photoUrl = user.photoUrl
+
+            // Check if user's email is verified
+            val emailVerified = user.isEmailVerified
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            val uid = user.uid
+        }
+    }
+
     fun myToast(context: Context, text: String){
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
