bdb812a9651d4e39547819b15c3190a4d53b2039
abdulmughnialfikri <dev.abdulmughni@gmail.com>
2020-08-02 01:40:50 +0700

    home layout

--- app/src/main/java/id/co/personal/pasarikan/fragment/HomeFragment.kt
+++ app/src/main/java/id/co/personal/pasarikan/fragment/HomeFragment.kt
@@ -7,6 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
@@ -14,6 +18,8 @@ import com.google.firebase.ktx.Firebase
 import id.co.personal.pasarikan.R
 import id.co.personal.pasarikan.adapter.ItemAdapter
 import id.co.personal.pasarikan.models.Item
+import id.co.personal.pasarikan.models.User
+import kotlinx.android.synthetic.main.activity_edit_profile.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
@@ -21,6 +27,9 @@ class HomeFragment : Fragment() {
     private lateinit var listItemItem: ArrayList<Item>
     private var database: FirebaseDatabase = Firebase.database
     private lateinit var dbRef: DatabaseReference
+    private var currentUser: FirebaseUser? = null
+    private var uid: String = ""
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,57 +37,51 @@ class HomeFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
+    init {
+        dbRef = database.getReference("users")
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        readUserProfile()
+        auth = FirebaseAuth.getInstance()
+        getUserId()
         listItemItem = ArrayList()
         listItemItem.clear()
         getItem()
-        pb_list_item_h.visibility = View.VISIBLE
     }
-
-    private fun readUserProfile() {
-        val database = Firebase.database
-        dbRef = database.getReference("users")
+    private fun getUserId(){
+        currentUser = auth.currentUser
+        currentUser?.let {
+            uid = currentUser!!.uid
+            readUserProfile(uid)
+        }
+    }
+    private fun readUserProfile(uid: String) {
         val userListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 
             }
+
             override fun onCancelled(databaseError: DatabaseError) {
                 Log.w("DataChange", "loadPost:onCancelled", databaseError.toException())
             }
         }
-        dbRef.child("11").addValueEventListener(userListener)
+        dbRef.child(uid).addListenerForSingleValueEvent(userListener)
     }
 
     private fun getItem(){
         dbRef = database.getReference("items")
         val itemListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                pb_list_item_h.visibility = View.VISIBLE
                 listItemItem.clear()
                 for (postSnapshot in dataSnapshot.children) {
                     val items = postSnapshot?.getValue<Item>()
                     items?.let { it1 -> listItemItem.add(it1) }
                 }
-                pb_list_item_h.visibility = View.INVISIBLE
-                showRecyclerList()
+                //showRecyclerList()
             }
             override fun onCancelled(databaseError: DatabaseError) {
                 Log.w("DataChange", "loadPost:onCancelled", databaseError.toException())
             }
         }
         dbRef.addValueEventListener(itemListener)
-    }
-
-    private fun showRecyclerList(){
-        listItemItem.reverse()
-        val adapter = ItemAdapter (context!!, listItemItem)
-        rv_fish_item.layoutManager = LinearLayoutManager(
-            context,
-            LinearLayoutManager.VERTICAL,
-            false
-        )
-        rv_fish_item.adapter = adapter
     }
 }
